2f78cb0e7643faca5784599ce9f6283410cbba2e
CuteITGuy <grisetroseciel@gmail.com>
2016-03-04 19:42:36 +0700

    First commit

---
+++ CB.Media.Brushes/Impl/ColorHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+
+
+namespace CB.Media.Brushes.Impl
+{
+    internal class ColorHelper
+    {
+        #region Implementation
+        internal static double CalculateBrightness(Color selectedColor)
+        {
+            return (0.299 * selectedColor.R + 0.587 * selectedColor.G + 0.114 * selectedColor.B) / 255;
+        }
+        #endregion
+    }
+}
